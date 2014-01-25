2a3aad3a1d4aa28c79c5a8b3d9728f7b0291867a
Flora Jin <florajin14@gmail.com>
2014-01-24 22:16:10 -0500

    added some hot pocket detector shit

---
+++ PitaBread/PTColorHistogram.h
@@ -0,0 +1,13 @@
+//
+//  PTColorHistogram.h
+//  PitaBread
+//
+//  Created by Flora on 1/24/14.
+//  Copyright (c) 2014 Team Name Optional. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PTColorHistogram : NSObject
+
+@end
