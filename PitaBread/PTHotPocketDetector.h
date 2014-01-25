8c4f3fe2a921a9bb319a1acfaa7ea7c21ed7df4d
Jackson Owens <jackson_owens@brown.edu>
2014-01-24 21:00:06 -0500

    Added hot pocket detector

---
+++ PitaBread/PTHotPocketDetector.h
@@ -0,0 +1,13 @@
+//
+//  PTHotPocketDetector.h
+//  PitaBread
+//
+//  Created by Jackson Owens on 1/24/14.
+//  Copyright (c) 2014 Team Name Optional. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PTHotPocketDetector : NSObject
+
+@end
---
+++ PitaBread/PTHotPocketDetector.m
@@ -0,0 +1,13 @@
+//
+//  PTHotPocketDetector.m
+//  PitaBread
+//
+//  Created by Jackson Owens on 1/24/14.
+//  Copyright (c) 2014 Team Name Optional. All rights reserved.
+//
+
+#import "PTHotPocketDetector.h"
+
+@implementation PTHotPocketDetector
+
+@end
