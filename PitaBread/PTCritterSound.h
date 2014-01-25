63d0d47d572a0bf428ad3fdc4f339c33067c9bba
Flora Jin <florajin14@gmail.com>
2014-01-25 11:27:54 -0500

    added music stencils

---
+++ PitaBread/PTCritterSound.h
@@ -0,0 +1,13 @@
+//
+//  PTCritterSound.h
+//  PitaBread
+//
+//  Created by Flora on 1/25/14.
+//  Copyright (c) 2014 Team Name Optional. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PTCritterSound : NSObject
+
+@end
---
+++ PitaBread/PTCritterSound.m
@@ -0,0 +1,13 @@
+//
+//  PTCritterSound.m
+//  PitaBread
+//
+//  Created by Flora on 1/25/14.
+//  Copyright (c) 2014 Team Name Optional. All rights reserved.
+//
+
+#import "PTCritterSound.h"
+
+@implementation PTCritterSound
+
+@end
