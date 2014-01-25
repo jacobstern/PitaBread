7222ba029ed130fff7898ef8a11d71aba75b5b6a
Jacob Stern <jacob.stern@outlook.com>
2014-01-24 22:51:28 -0500

    Add PTCritterSprite

---
+++ PitaBread/PTCritterSprite.h
@@ -0,0 +1,15 @@
+//
+//  PTCritterSprite.h
+//  PitaBread
+//
+//  Created by Jacob Stern on 1/24/14.
+//  Copyright (c) 2014 Team Name Optional. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface PTCritterSprite : SKSpriteNode
+
++ (PTCritterSprite *)withTextureAtlas:(SKTextureAtlas *)atlas;
+
+@end
